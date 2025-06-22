659db96478460d05ce6b47fbcbc1557d9fb58bac
amnamum <1211111919@student.mmu.edu.my>
2025-06-23 06:24:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ class App{
 
     this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 500);
     this.camera.position.set(0, 1.6, 0);
-		this.camera.lookAt(0, 1.5, -3); // Face the interactive box
+		this.camera.lookAt(0, 1.5, -3);
 
 
     const listener = new THREE.AudioListener();
